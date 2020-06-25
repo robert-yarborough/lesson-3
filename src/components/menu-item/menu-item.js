e399a5f2f3a1dcb993c83f3812c0b467cd7cf8a7
robert-yarborough <admin@robert-yarborough.com>
2020-06-25 17:59:58 -0400

    broken homepage into smaller components, finished styling menu-item component

--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './menu-item.styles.scss';
+import './menu-item.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
   <div className={`${size} menu-item`}>
